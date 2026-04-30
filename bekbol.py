a5bdca012baa64b9b4fcfc3de4903c2c917358a8
bekbol017 <aldybai.bekbol32@gmail.com>
2026-05-01 02:38:27 +0500

    Hello

--- bekbol.py
+++
@@ -1,9 +0,0 @@
-#Read two lines: a string S and a pattern P (a literal string without regex symbols). Using re.findall() or re.finditer(), count how many times P appears in S as a non-overlapping match.
-
-
-import re
-aru = input()
-aiz = re.findall(r"\d",aru)
-
-
-print(" ".join(aiz))
